954e69365f36e28fa26959cdd9efa187c737c6e4
shubankardey <157878776+shubankardey@users.noreply.github.com>
2024-01-30 03:08:37 +0530

    Add files via upload

--- Details.cpp
+++ Details.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
 
+//string name
+string name;
+cout<<"What is your name? ";
+cin>>name;
+cout<<"My name is "<<name<<endl;
+
+//Integer tytpes
+int age;
+cout<<"What is your age? ";
+cin>>age;
+cout<<"My age is- "<<age<<endl;
+
+//Character Value
+char gender;
+cout<<"What is your gender? ";
+cin>>gender;
+cout<<"My gender is "<<gender<<endl;
+
+//integer value
+int weight;
+cout<<"What is your weight? ";
+cin>>weight;
+cout<<"My weight is- "<<weight<<endl;
+
+
+return 0;
+}
